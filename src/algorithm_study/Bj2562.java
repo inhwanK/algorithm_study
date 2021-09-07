f9d9c383a5dcba67ce588683f1518add7d5d6823
HUSTAR14 <HUSTAR14@DESKTOP-FBK87Q3>
2021-09-07 19:15:53 +0900

    알고리즘 공부 첫 커밋.

---
+++ src/algorithm_study/Bj2562.java
@@ -0,0 +1,12 @@
+package algorithm_study;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Bj2562 {
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		
+	}
+}
