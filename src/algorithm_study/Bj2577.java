85213553ce42f4f08e8c7c6031972843370a8127
HUSTAR14 <HUSTAR14@DESKTOP-FBK87Q3>
2021-09-09 11:12:11 +0900

    2577번 문제 풀었음.

--- src/algorithm_study/Bj2577.java
+++ src/algorithm_study/Bj2577.java
@@ -1,57 +1,35 @@
 package algorithm_study;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Bj2577 {
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Scanner sc = new Scanner(System.in);
 
-		int A = Integer.parseInt(br.readLine());
-		int B = Integer.parseInt(br.readLine());
-		int C = Integer.parseInt(br.readLine());
-
-		br.close();
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
 
 		int value = A * B * C;
-		System.out.println(value);
-		String answer = value + "";
-		int num = 0;
-		int[] count = new int[9];
-
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		// 불필요한 반복이 있음.
-		// 다른 자료구조를 활용해야함.
+		String answer = Integer.toString(value);
+		int key = 0;
 		
-		Map<Integer, Integer> countMap = new HashMap<Integer, Integer>();
-		System.out.println(countMap);
-		
-//		countMap.get(countMap)
-		for (int i = 1; i < 10; i++) {
-			for (int j = 0; j < answer.length(); j++) {
-				num = answer.charAt(j) - '0';
-				if(countMap.containsKey(num)) {
-					int tmp = countMap.get(num)+1;
-					countMap.replace(num, tmp);
-				}else {
-					countMap.put(num, 1);
-				}
-					
-				if (i == num) {
-					count[i - 1]++;
-				}
-			}
+		Map<Integer, Integer> numCount = new HashMap<Integer, Integer>();
+
+		for (int i = 0; i < 10; i++) {
+			numCount.put(i, 0);
 		}
 		
+		for (int num = 0; num < answer.length(); num++) {
+			key = answer.charAt(num)- '0';
+			numCount.replace(key, numCount.get(key) +1);
+		}
 		
-		bw.flush();
-		bw.close();
-		br.close();
+		for(int i = 0; i < 10; i++) {
+			System.out.println(numCount.get(i));
+		}
 	}
 }
