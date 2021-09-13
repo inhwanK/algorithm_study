b0eb4512f4a18b42595461fe9eac1f3ef624a624
HUSTAR14 <HUSTAR14@DESKTOP-FBK87Q3>
2021-09-13 18:54:38 +0900

    1546 문제 해결

--- src/algorithm_study/Bj3052.java
+++ src/algorithm_study/Bj3052.java
@@ -3,12 +3,25 @@ package algorithm_study;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.HashSet;
 
 
 
 public class Bj3052 {
 	public static void main(String[] args) throws IOException {
+		
+		// Set을 이용한 풀이.
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int value = 0;
+		
+		HashSet<Integer> set = new HashSet<Integer>();
+		
+		for(int i = 0; i < 10; i++) {
+			value = Integer.valueOf(br.readLine());
+			set.add(value%42);
+		}
+		System.out.println(set.size());
+		
 		
 		/* 정렬을 이용한 풀이
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
---
+++ src/algorithm_study/Bj8958.java
@@ -0,0 +1,39 @@
+package algorithm_study;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Bj8958 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int num = Integer.valueOf(br.readLine());
+		ArrayList<Character> arr = new ArrayList<Character>();
+		String test = null;
+		int count =0;
+		int sum = 0;
+		
+		for (int i = 0; i < num; i++) {
+			test = br.readLine();
+			for (int j = 0; j < test.length(); j++) {
+				arr.add(test.charAt(j));
+			}
+			count = 0;
+			sum = 0;
+			for(int a = 0; a< arr.size();a++){
+				
+				if(arr.get(a).equals('O')){
+					count++;
+					
+				}else{
+					count = 0;
+				}
+				sum = sum + count;
+			}
+			System.out.println(sum);
+		}
+		br.close();
+	}
+}
