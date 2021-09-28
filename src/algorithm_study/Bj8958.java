efab370a56b187525fdb7a9f99769dac7e4d0689
inhwanK <79566221+inhwanK@users.noreply.github.com>
2021-09-28 15:41:34 +0900

    Update Bj8958.java

--- src/algorithm_study/Bj8958.java
+++ src/algorithm_study/Bj8958.java
@@ -5,7 +5,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
-// Ŀ�� �׽�Ʈ
+// Ä¿¹Ô Å×½ºÆ®
+// 흠...
 public class Bj8958 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
