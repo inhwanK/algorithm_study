6afb32824aa43a7441553e5a3db0fe9f434e98e1
inhwanK <inhan1009@naver.com>
2021-10-01 12:46:35 +0900

    getBytes() 사용, for-each 문 사용해서 쉽게 푸는 방법 주석.

--- src/algorithm_study/Bj8958.java
+++ src/algorithm_study/Bj8958.java
@@ -42,5 +42,35 @@ public class Bj8958 {
 		}
 		
 		System.out.print(sb);
+		
+		/*
+		 * 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		StringBuilder sb = new StringBuilder();
+ 
+		int test_case = Integer.parseInt(br.readLine()); // 테스트 케이스
+ 
+		for (int i = 0; i < test_case; i++) {
+ 
+			int cnt = 0; // 연속횟수
+			int sum = 0; // 누적 합산
+ 
+			for (byte value : br.readLine().getBytes()) {
+				
+				if (value == 'O') {
+					cnt++;
+					sum += cnt;
+				} 
+				else {
+					cnt = 0;
+				}
+			}
+			
+			sb.append(sum).append('\n');
+ 
+		}
+		System.out.println(sb);
+		 */
 	}
 }
