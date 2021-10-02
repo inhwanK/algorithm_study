a7ed2b68f92e419ecaf9464a517d8f48bcf4be7c
82109 <82109@DESKTOP-J831AQ7>
2021-10-02 20:26:40 +0900

    왜 컨트리뷰션 안뜰까..

--- src/algorithm_study/Bj4344.java
+++ src/algorithm_study/Bj4344.java
@@ -1,10 +1,13 @@
 package algorithm_study;
 
+import java.util.Scanner;
+
 // 평균은 넘겠지
+// 왜이럴까
 public class Bj4344 {
 	public static void main(String[] args) {
 		
-		
+		Scanner sc = new Scanner(System.in);
 		
 	}
 }
