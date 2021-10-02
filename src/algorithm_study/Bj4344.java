aacf6956d7ffd4a24755117836c18dcc97f9f0f4
82109 <82109@DESKTOP-J831AQ7>
2021-10-02 20:38:17 +0900

    .

--- src/algorithm_study/Bj4344.java
+++ src/algorithm_study/Bj4344.java
@@ -4,10 +4,11 @@ import java.util.Scanner;
 
 // 평균은 넘겠지
 // 왜이럴까
+// 후...
 public class Bj4344 {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		
+		int a = 0;
 	}
 }
