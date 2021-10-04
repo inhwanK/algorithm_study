683a6a5f0427c58b0a2491d1e349215c8fe16a02
82109 <inhan1009@naver.com>
2021-10-04 21:39:25 +0900

    백준 4344 문제 푸는중.

--- src/algorithm_study/Bj4344.java
+++ src/algorithm_study/Bj4344.java
@@ -1,14 +1,33 @@
 package algorithm_study;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 // 평균은 넘겠지
-// 커밋 테스트
-// ㅎ
+
 public class Bj4344 {
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		int a = 0;
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.valueOf(br.readLine());
+		String[] nList = new String[N];
+
+		for (int n = 0; n < N; n++) {
+			nList[n] = br.readLine();
+		}
+
+		StringTokenizer st;
+
+		for (int i = 0; i < nList.length; i++) {
+			st = new StringTokenizer(nList[i], " ");
+			String nSub = st.nextToken();
+
+			for (int j = 0; j < st.countTokens(); j++) {
+
+			}
+		}
 	}
 }
