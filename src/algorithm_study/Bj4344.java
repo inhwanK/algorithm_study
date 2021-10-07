024b613db7740fc376ce753f8d3663ab913ec051
inhwanK <inhan1009@naver.com>
2021-10-07 17:18:19 +0900

    왜 안될까남

--- src/algorithm_study/Bj4344.java
+++ src/algorithm_study/Bj4344.java
@@ -5,7 +5,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-// �룊洹좎� �꽆寃좎�
+// 평균은 넘겠지
+// encoding 방식 바꿈.
 
 public class Bj4344 {
 	public static void main(String[] args) throws IOException {
