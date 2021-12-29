6954b1310b17411bd27d55d6a109b86b6c033703
inhwanK <inhan1009@naver.com>
2021-12-29 20:00:08 +0900

    P92_ans 추가, for문 없이 품.

--- src/algorithm_study/hustar/P92.java
+++ src/algorithm_study/hustar/P92.java
@@ -10,7 +10,8 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.StringTokenizer;
 
-public class p92 {
+// 예제문제 포함 대략 100문제 있음.
+public class P92 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -26,35 +27,23 @@ public class p92 {
 		int M = Integer.parseInt(m);
 		int K = Integer.parseInt(k);
 
-		System.out.println("> " + N);
-		System.out.println("> " + M);
-		System.out.println("> " + K);
-
 		st = new StringTokenizer(br.readLine(), " ");
 
-		ArrayList<Integer> arr = new ArrayList<Integer>();
-		for (int j = 0; j < N; j++) {
-			arr.add(Integer.parseInt(st.nextToken()));
+		int[] arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		arr.sort(Collections.reverseOrder());
 
-		System.out.println("> " + arr.get(0));
-		System.out.println("> " + arr.get(1));
-		System.out.println("> " + arr.get(2));
+		Arrays.sort(arr);
+		int first = arr[N - 1];
+		int second = arr[N - 2];
 
-		int sum = 0;
-		int count = 0;
+		int das = first * K + second;
 
-		for (int i = 0; i < M; i++) {
+		int multi = M / (K + 1);
+		int addition = first * (M % (K + 1));
 
-			if (count != K) {
-				sum += arr.get(0);
-				count++;
-			} else {
-				sum += arr.get(1);
-				count = 0;
-			}
-		}
+		int sum = das * multi + addition;
 
 		bw.write("정답 > " + sum);
 
---
+++ src/algorithm_study/hustar/P92_ans.java
@@ -0,0 +1,64 @@
+package algorithm_study.hustar;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class P92_ans {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st;
+
+		st = new StringTokenizer(br.readLine(), " ");
+		String n = st.nextToken();
+		String m = st.nextToken();
+		String k = st.nextToken();
+
+		int N = Integer.parseInt(n);
+		int M = Integer.parseInt(m);
+		int K = Integer.parseInt(k);
+
+		System.out.println("> " + N);
+		System.out.println("> " + M);
+		System.out.println("> " + K);
+
+		st = new StringTokenizer(br.readLine(), " ");
+
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+		for (int j = 0; j < N; j++) {
+			arr.add(Integer.parseInt(st.nextToken()));
+		}
+		arr.sort(Collections.reverseOrder());
+
+		System.out.println("> " + arr.get(0));
+		System.out.println("> " + arr.get(1));
+		System.out.println("> " + arr.get(2));
+
+		int sum = 0;
+		int count = 0;
+
+		for (int i = 0; i < M; i++) {
+
+			if (count != K) {
+				sum += arr.get(0);
+				count++;
+			} else {
+				sum += arr.get(1);
+				count = 0;
+			}
+		}
+
+		bw.write("정답 > " + sum);
+
+		br.close();
+		bw.close();
+	}
+}
