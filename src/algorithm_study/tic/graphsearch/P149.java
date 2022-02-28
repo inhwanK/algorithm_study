127b30b684fc94e317e332e5804d803a2d4967d9
inhwanK <inhan1009@naver.com>
2022-02-28 20:24:17 +0900

    [이코테] 음료수 얼려먹기 푸는 중.

--- src/algorithm_study/tic/graphsearch/P149.java
+++ src/algorithm_study/tic/graphsearch/P149.java
@@ -1,5 +1,53 @@
 package algorithm_study.tic.graphsearch;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Stack;
+
 public class P149 {
 
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		// 1 <= N, M <= 1000
+		int N, M;
+
+		N = sc.nextInt();
+		M = sc.nextInt();
+
+		int[][] ice = new int[N][M];
+
+		// 배열에 값 넣기
+		for (int n = 0; n < N; n++) {
+			for (int m = 0; m < M; m++) {
+				ice[n][m] = sc.nextInt();
+			}
+		}
+
+		// 배열 확인
+		System.out.println("배열 함 볼까 > " + ice[0][1]);
+
+		// 스택이냐 큐냐
+		Stack<Integer> connIce = new Stack<Integer>();
+
+		// 시작 요소 ice[0][0]
+		// 열 값을 우선으로 탐색
+		
+		// 배열 탐색
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				
+				if(ice[i][j] == 1) {
+//					ice[i][j] = 
+				}
+				
+			}
+		}
+		
+	}
 }
