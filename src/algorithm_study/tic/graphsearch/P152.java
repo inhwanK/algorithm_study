16d5d324d00492f479f87b09a1fcb9c8e37287bc
inhwanK <inhan1009@naver.com>
2022-03-01 21:07:16 +0900

    미로 탈출 푸는 중.

---
+++ src/algorithm_study/tic/graphsearch/P152.java
@@ -0,0 +1,83 @@
+package algorithm_study.tic.graphsearch;
+
+import java.util.Scanner;
+
+public class P152 {
+
+	private static int N, M;
+	private static int[][] maze;
+	private static int result = 0;
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		N = sc.nextInt();
+		M = sc.nextInt();
+		sc.nextLine();
+
+		String row;
+		maze = new int[N][M];
+
+		for (int n = 0; n < N; n++) {
+
+			row = sc.nextLine();
+			for (int m = 0; m < M; m++) {
+				maze[n][m] = row.charAt(m) - '0';
+			}
+		} // end of for{}
+
+		sc.close();
+
+		int shortRoad = 0;
+		int result = getDirections(shortRoad, 0, 0);
+
+		System.out.println("답은? > " + result);
+	}
+
+//	
+//	길찾기 메서드
+	public static int getDirections(int shortRoad, int n, int m) {
+
+		if (n == N - 1 && m == M - 1) {
+			return shortRoad + 1;
+		} else if (n >= N || n < 0 || m >= M || m < 0) {
+			return shortRoad;
+		}
+
+		if (maze[n][m] == 1) {
+
+			shortRoad++;
+			maze[n][m] = 0;
+
+			System.out.println("여기 거치냐?");
+			int roadCheck = shortRoad;
+
+			// 빠른길 먼저
+			int right = getDirections(shortRoad, n, m + 1);
+			int down = getDirections(shortRoad, n + 1, m);
+
+			if (right < down) {
+				shortRoad = right;
+			} else {
+				shortRoad = down;
+			}
+
+			if (shortRoad == roadCheck) {
+				int left = getDirections(shortRoad, n, m - 1);
+				int up = getDirections(shortRoad, n - 1, m);
+
+				if (left < up) {
+					shortRoad = left;
+				} else {
+					shortRoad = up;
+				}
+			}
+
+			return shortRoad;
+		} else {
+			return shortRoad;
+		}
+
+	}
+}
