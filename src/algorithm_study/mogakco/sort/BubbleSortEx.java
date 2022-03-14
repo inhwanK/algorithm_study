1dd92bc7236f19318f136fcac58c3c9f18064284
inhwanK <inhan1009@naver.com>
2022-03-14 16:49:00 +0900

    Chore(BubbleSortEx) : 추가 배열 출력 코드

--- src/algorithm_study/mogakco/sort/BubbleSortEx.java
+++ src/algorithm_study/mogakco/sort/BubbleSortEx.java
@@ -1,4 +1,4 @@
-package algorithm_study.mogakco.bubblesort;
+package algorithm_study.mogakco.sort;
 
 public class BubbleSortEx {
     public static void main(String[] args) {
@@ -8,18 +8,18 @@ public class BubbleSortEx {
 
             int temp = 0;
             // 요소 비교 후 정렬 수행
-            for (int j = 0; j < arr.length - 1; j++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
 
                 if (arr[j] > arr[j + 1]) {
                     temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
                 }
-
+                System.out.println("배열 스캔 중 > " + arr[0] + "," + arr[1] + "," + arr[2] + "," + arr[3] + "," + arr[4]);
             }
 
-            System.out.println("배열 절렬 중 > " + arr[0] + "," + arr[1] + "," + arr[2] + "," + arr[3] + "," + arr[4]);
+            System.out.println("배열 정렬 중 > " + arr[0] + "," + arr[1] + "," + arr[2] + "," + arr[3] + "," + arr[4]);
         } // 이중 for 문 끝
-        System.out.println("배열 절렬 끝 > " + arr[0] + "," + arr[1] + "," + arr[2] + "," + arr[3] + "," + arr[4]);
+        System.out.println("배열 정렬 끝 > " + arr[0] + "," + arr[1] + "," + arr[2] + "," + arr[3] + "," + arr[4]);
     }
 }
