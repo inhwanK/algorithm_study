17ef1047bf2f3c4a7976273c4747b10a1611a03d
inhwanK <inhan1009@naver.com>
2022-03-14 12:48:53 +0900

    Feat(BubbleSortEx) : 추가 버블정렬 수행 코드

---
+++ src/algorithm_study/mogakco/bubblesort/BubbleSortEx.java
@@ -0,0 +1,26 @@
+package algorithm_study.mogakco.bubblesort;
+
+public class BubbleSortEx {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 9, 1, 6};
+
+        for (int i = 0; i < arr.length; i++) { // 배열의 요소 개수 만큼 스캔 수행
+
+            int temp = 0;
+
+            // 요소 비교 후 정렬 수행
+            for (int j = 0; j < arr.length - 1; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+
+            }
+
+            System.out.println("배열 절렬 중 > " + arr[0] + "," + arr[1] + "," + arr[2] + "," + arr[3] + "," + arr[4]);
+        } // 이중 for 문 끝
+        System.out.println("배열 절렬 끝 > " + arr[0] + "," + arr[1] + "," + arr[2] + "," + arr[3] + "," + arr[4]);
+    }
+}
