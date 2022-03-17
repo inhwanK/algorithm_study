9e045cf74f6a33e1765e51ef3cac98d7819712c7
inhwanK <inhan1009@naver.com>
2022-03-17 19:25:38 +0900

    Refactor(EratosEx) : 메서드 분리

--- src/algorithm_study/mogakco/prime/EratosEx.java
+++ src/algorithm_study/mogakco/prime/EratosEx.java
@@ -1,19 +1,60 @@
 package algorithm_study.mogakco.prime;
 
 import java.util.Scanner;
-import java.util.Set;
 
 public class EratosEx {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt() + 1;
-        int range = n - 1;
-        int sqrt = (int) Math.sqrt(n) + 1;
-        int count = 0;
+        int n = sc.nextInt();
 
-        boolean[] primeList = new boolean[n];
-        for (int i = 0; i < n; i++) {
+        boolean[] primeList = checkPrimeNumber(n);
+
+        int count = countPrimeNumber(n, primeList);
+
+        System.out.println(n + " 이하의 소수의 수 : " + count);
+
+        int sum = 0;
+        String sumList = "[";
+        for (int i = 2; i < n; i++) {
+
+            if (primeList[i]) {
+
+                sumList += "" + i;
+                sum += i;
+
+                if (sum == n) {
+                    sumList += "]";
+                    System.out.println("연속된 소수 " + sumList + "의 합은 " + n + "입니다.");
+                    break;
+                }
+                if (sum > n) {
+                    System.out.println("연속된 소수의 합으로 " + n + "을 만들 수 없습니다.");
+                    break;
+                }
+
+                sumList += ", ";
+            }
+        }
+    }
+
+    private static int countPrimeNumber(int n, boolean[] primeList) {
+        int count = 0;
+        for (int i = 2; i < n + 1; i++) {
+            if (primeList[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static boolean[] checkPrimeNumber(int n) {
+
+        int range = n + 1;
+        int sqrt = (int) Math.sqrt(range) + 1;
+
+        boolean[] primeList = new boolean[range];
+        for (int i = 0; i < range; i++) {
             primeList[i] = true;
         }
 
@@ -24,31 +65,7 @@ public class EratosEx {
                 }
             }
         }
-/*
-        for (int i = 2; i < n; i++) {
-            if (primeList[i]) {
-                count++;
-            }
-        }
-*/
-        int sum = 0;
-        for (int i = 2; i < n; i++) {
-            if (primeList[i]) {
 
-                sum += i;
-
-                if (sum == range) {
-                    System.out.println("연속된 소수로 " + range + "을 만들 수 있습니다.");
-                    break;
-                }
-                if (sum > range) {
-                    System.out.println("연속된 소수의 합으로 " + range + "을 만들 수 없습니다.");
-                    break;
-                }
-            }
-        }
-
-//        System.out.println(count);
-
+        return primeList;
     }
 }
