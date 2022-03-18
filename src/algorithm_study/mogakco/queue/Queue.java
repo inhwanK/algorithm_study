ad87b132a7aab203d680b6138a10775c7a2f1704
inhwanK <inhan1009@naver.com>
2022-03-19 01:12:49 +0900

    Feat(Queue) : 선형 Queue 구현

--- src/algorithm_study/mogakco/queue/Queue.java
+++ src/algorithm_study/mogakco/queue/Queue.java
@@ -2,8 +2,75 @@ package algorithm_study.mogakco.queue;
 
 public class Queue {
 
+    private int queSize = 5;
+    private int[] queue;
+    private int head;
+    private int tail;
 
     public Queue() {
+        head = -1;
+        tail = -1;
+        queue = new int[queSize];
+    }
 
+    boolean isFull() {
+        if (tail >= queSize - 1 && head == -1) {
+            return true;
+        }
+
+        return false;
+    }
+
+    boolean isEmpty(){
+        if(head == tail){
+            return true;
+        } else{
+            return false;
+        }
+
+    }
+
+    void enqueue(int item) {
+
+        if (!isFull()) {
+            tail = tail + 1;
+            queue[tail] = item;
+            System.out.println("삽입");
+            return;
+        }
+        System.out.println("큐가 꽉 차 있습니다. ");
+    }
+
+    int dequeue() {
+        if (head != tail) {
+            head++;
+            System.out.println("추출");
+            return queue[head];
+        }
+
+        System.out.println("큐가 비어있습니다.");
+        return -1;
+    }
+}
+
+class Main{
+    public static void main(String[] args) {
+        Queue queue = new Queue();
+
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        queue.enqueue(4);
+        queue.enqueue(5);
+
+        queue.dequeue();
+        queue.dequeue();
+        queue.dequeue();
+        queue.dequeue();
+        queue.dequeue();
+
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
     }
 }
