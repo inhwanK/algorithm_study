acaa30933670e94738949cf46db65ce11e64f4dc
inhwanK <inhan1009@naver.com>
2022-03-18 21:26:07 +0900

    Feat(Queue, Stack) : 클래스 추가

---
+++ src/algorithm_study/mogakco/queue/Queue.java
@@ -0,0 +1,9 @@
+package algorithm_study.mogakco.queue;
+
+public class Queue {
+
+
+    public Queue() {
+
+    }
+}
---
+++ src/algorithm_study/mogakco/stack/Stack.java
@@ -0,0 +1,7 @@
+package algorithm_study.mogakco.stack;
+
+public class Stack {
+    public static void main(String[] args) {
+
+    }
+}
