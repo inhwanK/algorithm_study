6bd2905ec1694345ef24672e5fecdc80c85fcdff
inhwanK <inhan1009@naver.com>
2022-03-30 21:09:15 +0900

    Feat(Quiz12_4) : Quiz12_4 클래스 추가

---
+++ src/algorithm_study/mogakco/binarysearchtree/Quiz12_4.java
@@ -0,0 +1,10 @@
+package algorithm_study.mogakco.binarysearchtree;
+
+public class Quiz12_4 {
+    public static void main(String[] args) {
+        int[] preOrder = {7, 3, 1, 5, 4, 12, 10, 8};
+        int[] inOrder = {1, 3, 12, 4, 5, 7, 8, 10};
+
+
+    }
+}
