b2f3070ced0f01a1e6725b8886f9d27057221122
inhwanK <inhan1009@naver.com>
2022-04-07 00:08:07 +0900

    Feat(P180) : [이코테] p180 풀이 답 추가 완료

---
+++ src/algorithm_study/tic/sort/P180.java
@@ -0,0 +1,64 @@
+package algorithm_study.tic.sort;
+
+import com.sun.xml.internal.ws.util.StreamUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class P180 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        List<Student> students = new ArrayList<Student>();
+
+        for (int i = 0; i < N; i++) {
+            String name = sc.next();
+            int score = sc.nextInt();
+
+            students.add(new Student(name, score));
+
+        }
+
+        Collections.sort(students);
+
+        for (int i = 0; i < students.size(); i++) {
+            System.out.print(students.get(i).getName() + " ");
+        }
+
+    }
+}
+
+class Student implements Comparable<Student> {
+
+    private String name;
+    private int score;
+
+    public Student(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    @Override
+    public int compareTo(Student o) {
+
+        if (this.score == o.getScore()) {
+            return 0;
+        } else if (this.score > o.getScore()) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+}
