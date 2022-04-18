e3070efcdb9c85649ce23397818868e7f37da91d
inhwanK <inhan1009@naver.com>
2022-04-18 21:15:57 +0900

    Feat(P300) : [이코테] 도시 분할 계획

---
+++ src/algorithm_study/tic/graphtheory/P300.java
@@ -0,0 +1,23 @@
+package algorithm_study.tic.graphtheory;
+
+import java.util.Scanner;
+
+public class P300 {
+
+    static int[] house = new int[100001];
+    static int[] route = new int[1000001];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        for (int i = 0; i < m; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int cost = sc.nextInt();
+        }
+
+    }
+}
