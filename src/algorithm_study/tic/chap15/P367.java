521daf5c8122ee96c16a82b6ca7a2f262f65a956
inhwanK <inhan1009@naver.com>
2022-04-30 03:23:45 +0900

    Chore(P367) : [이코테] 주석 추가, 파라미터 수정

--- src/algorithm_study/tic/chap15/P367.java
+++ src/algorithm_study/tic/chap15/P367.java
@@ -3,15 +3,15 @@ package algorithm_study.tic.chap15;
 import java.util.*;
 
 // 정렬된 배열에서 특정 수의 개수 구하기 풀이 코드
+// 순차 탐색을 쓰지 않고 구하기
 public class P367 {
 
-    static int n, x;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        n = sc.nextInt();
-        x = sc.nextInt();
+        int n = sc.nextInt();
+        int x = sc.nextInt();
 
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
@@ -52,18 +52,18 @@ public class P367 {
     // 재귀로 구현한 이분탐색
     static int binarySearch(int x, int[] arr, int start, int end) {
 
-        int mid = start + end / 2;
-
-        if (start >= end) {
+        if (start > end) {
             return -1;
         }
+
+        int mid = (start + end) / 2;
 
         if (x == arr[mid]) {
             return mid;
         } else if (arr[mid] < x) {
-            return binarySearch(x, arr, mid, end);
+            return binarySearch(x, arr, mid + 1, end);
         } else {
-            return binarySearch(x, arr, start, mid);
+            return binarySearch(x, arr, start, mid - 1);
         }
     }
 
