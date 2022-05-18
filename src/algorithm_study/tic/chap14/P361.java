76b6b739065a9b587ca6a758bc05c25780915b2d
inhwanK <79566221+inhwanK@users.noreply.github.com>
2022-05-18 10:45:55 +0900

    Feat(P361) : [이코테 - 실패율] 풀이 추가

---
+++ src/algorithm_study/tic/chap14/P361.java
@@ -0,0 +1,64 @@
+package algorithm_study.tic.chap14;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class P361 {
+    public int[] solution(int N, int[] stages) {
+        int[] answer = {};
+
+        int[] arr = new int[N + 1];
+        for(int i = 0; i < stages.length; i++){
+            if(stages[i] > N) continue;
+            else arr[stages[i]]++;
+        }
+
+        int challenge = stages.length;
+        List<Stage> stageList = new ArrayList<>();
+        for(int i = 1; i <= N; i++){
+
+            challenge = challenge - arr[i - 1];
+            float ratio = (float) arr[i] / challenge;
+
+            if(arr[i] == 0){
+                System.out.println(ratio);
+                stageList.add(new Stage(i, 0));
+                continue;
+            }
+
+            System.out.println("2 > "+ ratio);
+            stageList.add(new Stage(i, ratio));
+        }
+
+        Collections.sort(stageList, (o1, o2) -> Float.compare(o2.getRatio(), o1.getRatio()));
+
+        answer =  new int[stageList.size()];
+        for(int i = 0; i < stageList.size(); i++){
+            answer[i] = stageList.get(i).getStageNum();
+        }
+
+        return answer;
+    }
+
+}
+
+class Stage {
+
+    private int stageNum;
+    private float ratio;
+
+    public Stage(int stageNum, float ratio) {
+        this.stageNum = stageNum;
+        this.ratio = ratio;
+    }
+
+    public int getStageNum() {
+        return this.stageNum;
+    }
+
+    public float getRatio() {
+        return this.ratio;
+    }
+}
