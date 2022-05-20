1852023d508c7da34935e24dae3e6e9387437ede
inhwanK <79566221+inhwanK@users.noreply.github.com>
2022-05-20 23:45:22 +0900

    Feat(P363) : 카드 정렬하기 문제 풀이, 정답 코드 추가

---
+++ src/algorithm_study/tic/chap14/P363.java
@@ -0,0 +1,33 @@
+package algorithm_study.tic.chap14;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+// 카드 정렬하기 (큐와 힙 공부 더하기)
+public class P363 {
+
+    static int N;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+
+        PriorityQueue<Integer> cardHeap = new PriorityQueue();
+
+        for (int i = 0; i < N; i++) {
+            cardHeap.offer(sc.nextInt());
+        }
+
+        int result = 0;
+        while (cardHeap.size() != 1) {
+            int sum = cardHeap.poll() + cardHeap.poll();
+            cardHeap.offer(sum);
+            result += sum;
+
+        }
+
+        System.out.println(result);
+    }
+}
