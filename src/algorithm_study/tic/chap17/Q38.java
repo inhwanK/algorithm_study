c100364b70233cf1a7ddd6770788d7ca517c3f1f
inhwanK <inhan1009@naver.com>
2022-05-31 18:26:27 +0900

    Update(Q38) : 정확한 순위 풀이 중

--- src/algorithm_study/tic/chap17/Q38.java
+++ src/algorithm_study/tic/chap17/Q38.java
@@ -1,11 +1,22 @@
 package algorithm_study.tic.chap17;
 
 import java.util.Arrays;
-import java.util.PriorityQueue;
 import java.util.Scanner;
 
+/*
+6 6
+1 5
+3 4
+4 2
+4 6
+5 2
+5 4
+*/
 // 정확한 순위
 public class Q38 {
+
+    static final int INF = 999999999;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -15,15 +26,31 @@ public class Q38 {
         int[][] arr = new int[n + 1][n + 1];
 
         for (int i = 0; i < n; i++) {
-            Arrays.fill(arr[i], 0);
-            arr[i][i] = i;
-
+            Arrays.fill(arr[i], INF);
+            arr[i][i] = 0;
         }
 
-        PriorityQueue<Integer> heap = new PriorityQueue<>();
-
         for (int i = 0; i < m; i++) {
-//            heap.offer();
+            int col = sc.nextInt();
+            int row = sc.nextInt();
+
+            arr[col][row] = 1;
+        }
+
+        for (int i = 0; i < n + 1; i++) {
+            System.out.println(Arrays.toString(arr[i]));
+        }
+
+        for (int k = 1; k < n + 1; k++) {
+            for (int i = 1; i < n + 1; i++) {
+                for (int j = 1; j < n + 1; j++) {
+                    arr[i][j] = Math.min(arr[i][j], arr[i][k] + arr[k][j]);
+                }
+            }
+        }
+
+        for (int i = 0; i < n + 1; i++) {
+            System.out.println(Arrays.toString(arr[i]));
         }
     }
 }
