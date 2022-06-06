5c45486adfaa8a7a9b4d9376ea74656cda437c9c
inhwanK <79566221+inhwanK@users.noreply.github.com>
2022-06-06 16:28:16 +0900

    Feat(Q09) : [이코테 - 구현] 문자열 압축 풀이 추가

--- src/algorithm_study/tic/chap12/Q09.java
+++ src/algorithm_study/tic/chap12/Q09.java
@@ -2,7 +2,39 @@ package algorithm_study.tic.chap12;
 
 public class Q09 {
     public int solution(String s) {
-        int answer = 0;
+        int answer = s.length();
+
+        for (int i = 1; i <= s.length() / 2; i++) {
+
+            String compressed = "";
+            String prev = s.substring(0, i);
+            int count = 1;
+
+            for (int j = i; j < s.length(); j += i) {
+
+                String sub = "";
+                for (int k = j; k < j + i; k++) {
+                    if (k < s.length()) sub += s.charAt(k);
+                }
+
+                if (prev.equals(sub)) count += 1;
+                else {
+                    compressed += (count >= 2) ? count + prev : prev;
+                    sub = "";
+
+                    for (int k = j; k < j + i; k++) {
+                        if (k < s.length()) sub += s.charAt(k);
+                    }
+
+                    prev = sub;
+                    count = 1;
+                }
+            }
+
+            compressed += (count >= 2) ? count + prev : prev;
+            answer = Math.min(answer, compressed.length());
+        }
+
         return answer;
     }
 }
