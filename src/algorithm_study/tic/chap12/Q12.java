caba18fe66209d0d20414a41c1a8321fb89bf489
inhwanK <79566221+inhwanK@users.noreply.github.com>
2022-06-12 02:54:07 +0900

    Feat(Q12) : [이코테 - 구현] 기둥과 보 설치 클래스 추가

---
+++ src/algorithm_study/tic/chap12/Q12.java
@@ -0,0 +1,18 @@
+package algorithm_study.tic.chap12;
+
+public class Q12 {
+    public int[][] solution(int n, int[][] build_frame){
+        int[][] answer = {};
+
+        // 전체 구조물을 확인하는 메서드
+
+        // 설치 수행한 뒤 전체 구조 확인
+            // 구조가 잘 못 되었다면, 다시 삭제
+
+        // 삭제 수행한 뒤 전체 구조 확인
+            // 구조가 잘 못 되었다면, 다시 설치
+
+
+        return answer;
+    }
+}
