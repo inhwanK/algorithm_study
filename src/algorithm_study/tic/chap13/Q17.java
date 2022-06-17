07058fb37eb430f5dd0f70b39a88027e4f24f467
inhwanK <79566221+inhwanK@users.noreply.github.com>
2022-06-17 19:09:59 +0900

    Feat(Q17) : [이코테 - DFS/BFS] (경쟁적 전염) 문제 풀이 및 정답 추가

--- src/algorithm_study/tic/chap13/Q17.java
+++ src/algorithm_study/tic/chap13/Q17.java
@@ -1,7 +1,21 @@
 package algorithm_study.tic.chap13;
 
-import java.util.Scanner;
+import java.util.*;
 
+/*
+입력값
+3 3
+1 0 2
+0 0 0
+3 0 0
+2 3 2
+
+3 3
+1 0 2
+0 0 0
+3 0 0
+1 2 2
+*/
 // 경쟁적 전염
 public class Q17 {
     public static void main(String[] args) {
@@ -10,28 +24,81 @@ public class Q17 {
         int n = sc.nextInt();
         int k = sc.nextInt();
 
+        sc.nextLine();
         int[][] arr = new int[n][n];
+        ArrayList<Virus> virusList = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 arr[i][j] = sc.nextInt();
+                if (arr[i][j] != 0) virusList.add(new Virus(i, j, arr[i][j], 0));
             }
         }
 
+        sc.nextLine();
         int s = sc.nextInt();
         int x = sc.nextInt();
         int y = sc.nextInt();
 
-        int[] dx = {0, 1, 0, -1};
-        int[] dy = {1, 0, -1, 0};
+        // 상하좌우 증식
+        int[] dx = {0, 0, -1, 1};
+        int[] dy = {-1, 1, 0, 0};
 
-        // s초 만큼 반복
-            // 수를 증가시키면서 바이러스 찾기
+        Collections.sort(virusList);
+        // 증식 순서를 위한 큐
+        Queue<Virus> infection = new LinkedList<Virus>();
+        for (int i = 0; i < virusList.size(); i++) {
+            infection.offer(virusList.get(i));
+        }
+        // 큐가 빌 때까지 반복
+        while (!infection.isEmpty()) {
+            Virus virus = infection.poll();
+            if(virus.getSecond() == s) break;
 
-                // 증식하기 전 체크
-                    // 시험관을 벗어나는지 확인
-                    // 바이러스 존재 여부 확인
+            for (int i = 0; i < 4; i++) {
+                int nx = virus.getRow() + dx[i];
+                int ny = virus.getCol() + dy[i];
+                // 시험관을 벗어나는지 확인, 바이러스 존재 여부 확인
+                if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+                else if (arr[nx][ny] != 0) continue;
+                // 증식
+                arr[nx][ny] = virus.getType();
+                infection.offer(new Virus(nx, ny, virus.getType(), virus.getSecond() + 1));
+            }
+        }
+        System.out.println(arr[x - 1][y - 1]);
+    }
+}
 
-                // true 일 경우 증식
+class Virus implements Comparable<Virus> {
+    private int row;
+    private int col;
+    private int type;
+    private int second;
 
+    public Virus(int row, int col, int type, int second) {
+        this.row = row;
+        this.col = col;
+        this.type = type;
+        this.second = second;
+    }
+
+    public int getRow() {
+        return this.row;
+    }
+
+    public int getCol() {
+        return this.col;
+    }
+
+    public int getType() {
+        return this.type;
+    }
+
+    public int getSecond() {
+        return this.second;
+    }
+
+    public int compareTo(Virus other) {
+        return this.getType() - other.getType();
     }
 }
