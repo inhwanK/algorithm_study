c1fe787819efffcf5c300b45ffa5335a447dc6bd
inhwanK <79566221+inhwanK@users.noreply.github.com>
2022-06-27 22:24:06 +0900

    Feat(BOJ2941Answer) : 정답코드 추가

--- src/algorithm_study/boj/string/BOJ2941.java
+++ src/algorithm_study/boj/string/BOJ2941.java
@@ -29,3 +29,46 @@ public class BOJ2941 {
         br.close();
     }
 }
+
+// 이것도 바이트로 받아버려?
+// 상위 코드 - https://www.acmicpc.net/source/11463976
+class BOJ2941Answer {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String str = bufferedReader.readLine();
+        int count = 0;
+        for (int i = 1; i < str.length(); i++) {
+            char c = str.charAt(i);
+            switch (c) {
+                case '=':
+                    switch (str.charAt(i - 1)) {
+                        case 'c': case 's': count++; break;
+                        case 'z':
+                            if(i > 1 && str.charAt(i - 2) == 'd') {
+                                count += 2;
+                                break;
+                            } else {
+                                count ++;
+                                break;
+                            }
+                        default: break;
+                    }
+                    break;
+                case '-':
+                    switch (str.charAt(i - 1)) {
+                        case 'c': case 'd': count++; break;
+                        default: break;
+                    }
+                    break;
+                case 'j':
+                    switch (str.charAt(i - 1)) {
+                        case 'l': case 'n': count++; break;
+                        default: break;
+                    }
+                    break;
+                default: break;
+            }
+        }
+        System.out.print(str.length() - count);
+    }
+}
