99154fe1ee2595a1730c71eb3ca169478b8fa638
inhwanK <79566221+inhwanK@users.noreply.github.com>
2022-06-27 22:07:57 +0900

    Feat(BOJ2941) : 풀이 추가

---
+++ src/algorithm_study/boj/string/BOJ2941.java
@@ -0,0 +1,32 @@
+package algorithm_study.boj.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+ljes=njak
+ddz=z=
+nljj
+c=c=
+dz=ak
+*/
+
+//크로아티아 알파벳 - https://www.acmicpc.net/problem/2941
+public class BOJ2941 {
+    public static String[] arr = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+//        int n = s.length();
+        for (int i = 0; i < arr.length; i++) {
+            s = s.replace(arr[i], "1");
+        }
+        System.out.println(s.length());
+
+        br.close();
+    }
+}
