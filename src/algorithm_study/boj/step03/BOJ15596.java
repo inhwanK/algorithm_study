4f404515211c35a04fcbea8b4519ce215f7ab05d
inhwanK <79566221+inhwanK@users.noreply.github.com>
2022-07-02 22:14:01 +0900

    Feat(BOJ15596) : 풀이, 정답 추가

---
+++ src/algorithm_study/boj/step03/BOJ15596.java
@@ -0,0 +1,13 @@
+package algorithm_study.boj.step03;
+
+// 1위 정답과 동일
+// 정수 N개의 합 - https://www.acmicpc.net/problem/15596
+public class BOJ15596 {
+    long sum(int[] a) {
+        int sum = 0;
+        for (int i = 0; i < a.length; i++) {
+            sum += a[i];
+        }
+        return sum;
+    }
+}
