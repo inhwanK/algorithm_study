f960c29bc9cf9f31ae9e9fd3a4c9ae1cbe6b1f55
inhwanK <79566221+inhwanK@users.noreply.github.com>
2022-07-02 15:45:29 +0900

    Feat(BOJ2839) : 코드 정리

--- src/algorithm_study/boj/basicmath1/BOJ2839.java
+++ src/algorithm_study/boj/basicmath1/BOJ2839.java
@@ -24,17 +24,17 @@ public class BOJ2839 {
 
 // 1위 정답 - https://www.acmicpc.net/source/6451515
 class BOJ2839Answer {
-    public static void main(String args[]) throws Exception	{
+    public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int input = Integer.parseInt(br.readLine());
         int num = 0;
-        while(input%5!=0){
+        while (input % 5 != 0) {
             input -= 3;
             num++;
         }
-        if(input < 0) System.out.println(-1);
-        else{
-            num += input/5;
+        if (input < 0) System.out.println(-1);
+        else {
+            num += input / 5;
             System.out.println(num);
         }
         br.close();
