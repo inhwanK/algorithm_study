6081297db4ca7c9e640b30405fd53cc82130ee5f
inhwanK <79566221+inhwanK@users.noreply.github.com>
2022-07-02 16:23:18 +0900

    Feat(step1) : 단계별 문제 안푼 거 마저 풀이 중 step1 완료

---
+++ src/algorithm_study/boj/step1/BOJ18108.java
@@ -0,0 +1,14 @@
+package algorithm_study.boj.step1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 1998년생인 내가 태국에서는 2541년생?! - https://www.acmicpc.net/problem/18108
+public class BOJ18108 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 543
+        System.out.println(Integer.parseInt(br.readLine()) - 543);
+    }
+}
---
+++ src/algorithm_study/boj/step1/BOJ25083.java
@@ -0,0 +1,14 @@
+package algorithm_study.boj.step1;
+
+// 1위 정답과 같음
+// 새싹 - https://www.acmicpc.net/problem/25083
+public class BOJ25083 {
+    public static void main(String[] args) {
+        System.out.println("         ,r\'\"7");
+        System.out.println("r`-_   ,\'  ,/");
+        System.out.println(" \\. \". L_r\'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.print("      |");
+    }
+}
