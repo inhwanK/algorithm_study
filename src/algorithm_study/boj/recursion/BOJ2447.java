997a645295efd9a7e8a83a0dbd922463433b32a8
inhwanK <inhan1009@naver.com>
2022-07-04 23:14:52 +0900

    Feat(BOJ2447) : 정답 추가

--- src/algorithm_study/boj/recursion/BOJ2447.java
+++ src/algorithm_study/boj/recursion/BOJ2447.java
@@ -1,9 +1,9 @@
 package algorithm_study.boj.recursion;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
+// 분할과 정복 알고리즘?
+// 별 찍기 - 10 - https://www.acmicpc.net/problem/2447
 public class BOJ2447 {
     static char[][] arr;
 
@@ -61,6 +61,50 @@ public class BOJ2447 {
                 } else {
                     star(i, j, size, false);
                 }
+            }
+        }
+    }
+}
+
+// 위 풀이랑 동일한 알고리즘
+// 상위 정답 - https://www.acmicpc.net/source/43051500
+class BOJ2447Answer {
+    public static char[][] star;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+
+        star = new char[n][n];
+
+        printStar(0, 0, n, true);
+        for (int i = 0; i < n; i++) {
+            bw.write(star[i]);
+            bw.write("\n");
+        }
+        bw.flush();
+    }
+
+    private static void printStar(int x, int y, int n, boolean isStar) {
+        if (!isStar) {
+            for (int i = x; i < x + n; i++) {
+                for (int j = y; j < y + n; j++) {
+                    star[i][j] = ' ';
+                }
+            }
+            return;
+        }
+
+        if (n == 1) {
+            star[x][y] = '*';
+            return;
+        }
+        int cnt = 0;
+        for (int i = x; i < x + n; i += n / 3) {
+            for (int j = y; j < y + n; j += n / 3) {
+                printStar(i, j, n / 3, cnt != 4);
+                cnt++;
             }
         }
     }
