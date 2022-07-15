8e5b36e1373669dbe4e541da43ff650bb0432506
inhwanK <79566221+inhwanK@users.noreply.github.com>
2022-07-15 16:24:37 +0900

    Feat(BOJ17427) : 풀이 및 정답 추가

--- src/algorithm_study/boj/BOJ17427.java
+++ src/algorithm_study/boj/BOJ17427.java
@@ -1,26 +1,43 @@
 package algorithm_study.boj;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
+// 문제 풀이 아이디어 - https://codejb.tistory.com/4
 // 약수의 합 2 - https://www.acmicpc.net/problem/17427
 public class BOJ17427 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
 
-        int gn = 1;
-        for (int x = 2; x <= n; x++) {
-            int fx = divisorSum(x);
-            gn += fx;
+        //  1~7까지 자연수 중에서 7의 배수의 개수 = 약수 7을 가진 값들의 개수 = 7을 약수로 갖는 값의 개수 = N/7
+        // overflow 발생할 수 있음.
+        long gn = 0;
+        for (int i = 1; i <= n; i++) {
+            gn += (n / i) * i;
         }
 
-        System.out.println(gn);
+        bw.write(gn + "");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
+
+// 풀이 거의 동일
+// 상위 정답 - https://www.acmicpc.net/source/24686438
+class BOJ17427Answer {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(bf.readLine());
+        System.out.println(yaksuSum(num));
     }
 
-    // 어떤 수의 약수의 합을 구한 값 즉 f(x)
-    public static int divisorSum(int x) {
-        return 0;
+    public static long yaksuSum(int a) {
+        long sum = 0;
+        for (int i = 1; i <= a; i++) {
+            sum += i * (a/i);
+        }
+        return sum;
     }
 }
