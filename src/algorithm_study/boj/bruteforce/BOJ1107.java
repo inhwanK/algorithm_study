987acd7a3e35981913c0a1119c5f4ae96c91d1e2
inhwanK <79566221+inhwanK@users.noreply.github.com>
2022-07-22 06:47:00 +0900

    Fix(BOJ1107) : 풀이 수정

--- src/algorithm_study/boj/bruteforce/BOJ1107.java
+++ src/algorithm_study/boj/bruteforce/BOJ1107.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+// 알고리즘 설계는 했으나 구현을 정확히 못함...
 // 리모컨 - https://www.acmicpc.net/problem/1107
 public class BOJ1107 {
     public static boolean[] broken = new boolean[10];
@@ -22,21 +23,43 @@ public class BOJ1107 {
             }
         }
 
-
+        int answer = Math.abs(100 - target);
+        if (in != 10) {
+            // target이 숫자 버튼으로 바로 가능한 경우
+            if (isPossible(target)) {
+                answer = Math.min(answer, String.valueOf(target).length());
+            } else { // 숫자 버튼으로 안되는 경우
+                // 인덱스를 늘리면서 숫자버튼으로 접근 가능한 숫자 확인
+                int idx = 1;
+                while (true) {
+                    if ((target - idx) >= 0 && isPossible(target - idx)) {
+                        answer = Math.min(answer, String.valueOf(target - idx).length() + idx);
+                        break;
+                    }
+                    if (isPossible(target + idx)) {
+                        answer = Math.min(answer, String.valueOf(target + idx).length() + idx);
+                        break;
+                    }
+                    idx++;
+                }
+            }
+        }
+        System.out.println(answer);
     }
 
     public static boolean isPossible(int num) {
         if (num < 0) return false;
 
-        String input = num + "";
+        String input = String.valueOf(num);
         for (int i = 0; i < input.length(); i++) {
             int index = input.charAt(i) - '0';
-            if (broken[index] == true) return false;
+            if (broken[index]) return false;
         }
         return true;
     }
 }
 
+// 상위 정답 - https://www.acmicpc.net/source/22225533
 class BOJ1107Answer {
     static boolean[] broken = new boolean[10];
 
