ca085d40af8f84bfc18ae3054396505d9e038923
inhwanK <inhan1009@naver.com>
2022-07-27 19:27:41 +0900

    Feat(BOJ15649) : 정답 추가

--- src/algorithm_study/boj/bruteforce/BOJ15649.java
+++ src/algorithm_study/boj/bruteforce/BOJ15649.java
@@ -3,6 +3,7 @@ package algorithm_study.boj.bruteforce;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 // N과 M (1) - https://www.acmicpc.net/problem/15649
 public class BOJ15649 {
@@ -19,16 +20,16 @@ public class BOJ15649 {
         m = Integer.parseInt(in[1]);
         visited = new boolean[n + 1];
 
-        int[] result = new int[m];
+        char[] result = new char[m];
         permutation(0, result);
         System.out.println(sb);
     }
 
     // 순열...
-    public static void permutation(int depth, int[] result) {
+    public static void permutation(int depth, char[] result) {
         if (depth == m) {
             for (int i = 0; i < m; i++) {
-                sb.append(result[i]).append(" ");
+                sb.append(result[i]).append(' ');
             }
             sb.append('\n');
             return;
@@ -37,9 +38,52 @@ public class BOJ15649 {
         for (int i = 1; i <= n; i++) {
             if (!visited[i]) {
                 visited[i] = true;
-                result[depth] = i;
+                result[depth] = (char) (i + '0');
                 permutation(depth + 1, result);
                 visited[i] = false;
+            }
+        }
+    }
+}
+
+// 내 풀이와 큰 차이 없음.. 배열로 띄어쓰기 구현한 정도?
+// 상위 정답 - https://www.acmicpc.net/source/41491908
+class BOJ15649Answer {
+    static int N, M;
+    static boolean[] check;
+    static char[] answer;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        check = new boolean[N + 1];
+        answer = new char[2 * M];
+
+        for (int i = 0; i < M - 1; i++)
+            answer[2 * i + 1] = ' ';
+
+        answer[2 * M - 1] = '\n';
+        dfs(0);
+        System.out.println(sb);
+    }
+
+    static void dfs(int m) {
+        if (m == M) {
+            sb.append(answer);
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            if (!check[i]) {
+                check[i] = true;
+                answer[2 * m] = (char) (i + '0');
+                dfs(m + 1);
+                check[i] = false;
             }
         }
     }
