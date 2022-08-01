bb8409d3504a9eedd4183273e6577bd2843b5df3
inhwanK <79566221+inhwanK@users.noreply.github.com>
2022-08-01 19:22:07 +0900

    Feat(BOJ14500) : 풀이 중

--- src/algorithm_study/boj/bruteforce/BOJ14500.java
+++ src/algorithm_study/boj/bruteforce/BOJ14500.java
@@ -6,51 +6,52 @@ import java.io.*;
 // 테트로미노 - https://www.acmicpc.net/problem/14500
 public class BOJ14500 {
     public static int n, m;
+    public static int[] dx = {1, 0, -1, 0};
+    public static int[] dy = {0, 1, 0, -1};
+    public static int result = 0;
+
+    public static int[][] arr;
+    public static boolean[][] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String[] size = br.readLine().split(" ");
-        n = Integer.parseInt(size[0]);
-        m = Integer.parseInt(size[1]);
+        String[] input = br.readLine().split(" ");
+        n = Integer.parseInt(input[0]);
+        m = Integer.parseInt(input[1]);
 
-        int[][] board = new int[n][m];
+        arr = new int[n][m];
+        visited = new boolean[n][m];
         for (int i = 0; i < n; i++) {
             String[] row = br.readLine().split(" ");
             for (int j = 0; j < m; j++) {
-                board[i][j] = Integer.parseInt(row[j]);
+                arr[i][j] = Integer.parseInt(row[j]);
             }
         }
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
+    }
 
+    public static void calculateMaxValue(int depth, int x, int y, int sum) {
+        if (depth == 4) {
+            result = Math.max(result, sum);
+            return;
+        }
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
+                continue;
+            }
+
+            if (!visited[nx][ny]) {
+                visited[nx][ny] = true;
+                sum += arr[nx][ny];
+                calculateMaxValue(depth + 1, nx, ny, sum);
+                sum -= arr[nx][ny];
+                visited[nx][ny] = false;
             }
         }
     }
-
-    public static boolean validCheck(int i, int j) {
-        if (i < 0 || i >= n || j < 0 || j >= m) return false;
-        return true;
-    }
-
-    public static int rotateSum() {
-        int sum = 0;
-
-        return sum;
-    }
-
-    public static int symSum() {
-        return 0;
-    }
 }
-
-class Tetromino {
-    int[] x = new int[4];
-    int[] y = new int[4];
-
-    public void setX(int one, int two, int three, int four) {
-
-    }
-}
-
