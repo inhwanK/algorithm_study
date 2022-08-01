db08642addb3f293e1c026fc9cc4d9961eb63c5c
inhwanK <79566221+inhwanK@users.noreply.github.com>
2022-08-01 22:13:33 +0900

    Feat(BOJ14500) : 풀이 추가

--- src/algorithm_study/boj/bruteforce/BOJ14500.java
+++ src/algorithm_study/boj/bruteforce/BOJ14500.java
@@ -1,7 +1,8 @@
 package algorithm_study.boj.bruteforce;
 
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 // 테트로미노 - https://www.acmicpc.net/problem/14500
 public class BOJ14500 {
@@ -29,6 +30,15 @@ public class BOJ14500 {
             }
         }
 
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                initMaxValue(1, i, j, arr[i][j]);
+                visited[i][j] = true;
+                calculateMaxValue(1, i, j, arr[i][j]);
+                visited[i][j] = false;
+            }
+        }
+        System.out.println(result);
     }
 
     public static void calculateMaxValue(int depth, int x, int y, int sum) {
@@ -49,6 +59,30 @@ public class BOJ14500 {
                 visited[nx][ny] = true;
                 sum += arr[nx][ny];
                 calculateMaxValue(depth + 1, nx, ny, sum);
+                sum -= arr[nx][ny];
+                visited[nx][ny] = false;
+            }
+        }
+    }
+
+    public static void initMaxValue(int depth, int x, int y, int sum) {
+        if (depth == 4) {
+            result = Math.max(result, sum);
+            return;
+        }
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
+                continue;
+            }
+
+            if (!visited[nx][ny]) {
+                visited[nx][ny] = true;
+                sum += arr[nx][ny];
+                initMaxValue(depth + 1, x, y, sum);
                 sum -= arr[nx][ny];
                 visited[nx][ny] = false;
             }
