c66a83dd53d3667469d23746f48d66ed8fb1584b
inhwanK <79566221+inhwanK@users.noreply.github.com>
2022-08-05 03:01:25 +0900

    Feat(BOJ10972) : 풀이, 정답 추가

--- src/algorithm_study/boj/permutation/BOJ10972.java
+++ src/algorithm_study/boj/permutation/BOJ10972.java
@@ -1,7 +1,9 @@
 package algorithm_study.boj.permutation;
 
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 // 다음 순열 - https://www.acmicpc.net/problem/10972
 public class BOJ10972 {
@@ -17,49 +19,98 @@ public class BOJ10972 {
             arr[i] = Integer.parseInt(input[i]);
         }
 
-        int division = -1;
+        int divIndex = divArea();
+
+        if (divIndex == -1) {
+            System.out.println(divIndex);
+        } else {
+            for (int i = n - 1; i > 0; i--) {
+                if (arr[divIndex] < arr[i]) {
+                    int swap = arr[i];
+                    arr[i] = arr[divIndex];
+                    arr[divIndex] = swap;
+                    break;
+                }
+            }
+
+            int left = divIndex + 1;
+            int right = n - 1;
+
+            while (left < right) {
+                int swap = arr[left];
+                arr[left] = arr[right];
+                arr[right] = swap;
+
+                right--;
+                left++;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < n; i++) {
+                sb.append(arr[i]).append(" ");
+            }
+            System.out.println(sb);
+        }
+    }
+
+    private static int divArea() {
+        int index = -1;
         for (int i = n - 1; i > 0; i--) {
             if (arr[i - 1] < arr[i]) {
-                division = i - 1;
+                index = i - 1;
                 break;
             }
         }
+        return index;
+    }
+}
 
-        System.out.print(" > ");
+class BOJ10972Answer {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] array = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            System.out.print(arr[i] + " ");
+            array[i] = Integer.parseInt(st.nextToken());
         }
 
-        for (int i = n - 1; i > 0; i--) {
-            if (arr[division] < arr[i]) {
-                int swap = arr[i];
-                arr[i] = arr[division];
-                arr[division] = swap;
+        boolean[] visit = new boolean[n];
+        String answer = "";
+        int where = -1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (i == 0) {
+                answer = String.valueOf(-1);
                 break;
             }
-        }
 
-        System.out.print("\n");
-        for (int i = 0; i < n; i++) {
-            System.out.print(arr[i] + " ");
-        }
-
-        int left = division + 1;
-        int right = n - 1;
-
-        while (left < right) {
-            int swap = arr[left];
-            arr[left] = arr[right];
-            arr[right] = swap;
-
-            right--;
-            left++;
+            visit[array[i] - 1] = true;
+            if (array[i] > array[i - 1]) {
+                for (int j = array[i - 1]; j < n; j++) {
+                    if (visit[j]) {
+                        visit[array[i - 1] - 1] = true;
+                        array[i - 1] = j + 1;
+                        visit[j] = false;
+                        break;
+                    }
+                }
+                where = i;
+                break;
+            }
         }
 
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < n; i++) {
-            sb.append(arr[i]).append(" ");
+        if (where != -1) {
+            for (int i = 0; i < where; i++) {
+                sb.append(array[i]).append(" ");
+            }
+            for (int i = 0; i < n; i++) {
+                if (visit[i]) sb.append(i + 1).append(" ");
+            }
+            answer = sb.toString();
         }
-        System.out.println(sb);
+        System.out.println(answer);
     }
 }
