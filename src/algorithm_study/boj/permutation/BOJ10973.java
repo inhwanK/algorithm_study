8c37dd0fa0e752504e1740dba3534301948575ae
inhwanK <79566221+inhwanK@users.noreply.github.com>
2022-08-08 22:06:06 +0900

    Feat(BOJ10973) : 이전 순열 추가

---
+++ src/algorithm_study/boj/permutation/BOJ10973.java
@@ -0,0 +1,13 @@
+package algorithm_study.boj.permutation;
+
+import java.util.*;
+import java.io.*;
+
+// 이전 순열 - https://www.acmicpc.net/problem/10973
+public class BOJ10973 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+    }
+}
