4e44270ab36727c1c5fd8284dbf17b4ef80b1a65
inhwanK <79566221+inhwanK@users.noreply.github.com>
2022-08-11 02:39:36 +0900

    Feat(BOJ10971) : 정답 추가

--- src/algorithm_study/boj/permutation/BOJ10971.java
+++ src/algorithm_study/boj/permutation/BOJ10971.java
@@ -1,8 +1,6 @@
 package algorithm_study.boj.permutation;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 // 외판원 순회 2 - https://www.acmicpc.net/problem/10971
 public class BOJ10971 {
@@ -56,4 +54,57 @@ public class BOJ10971 {
             }
         }
     }
+}
+
+// 정답 - https://lotuslee.tistory.com/m/92?category=848933
+class BOJ10971Answer {
+    static int N;
+    static int[][] w;
+    static boolean[] visited;
+    static int min = Integer.MAX_VALUE;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        N = Integer.parseInt(br.readLine());
+        w = new int[N + 1][N + 1];
+        visited = new boolean[N + 1];
+        for (int i = 1; i <= N; i++) {
+            String[] sarr = br.readLine().split(" ");
+            for (int j = 1; j <= N; j++) {
+                w[i][j] = Integer.parseInt(sarr[j - 1]);
+            }
+        }
+
+        visited[1] = true;
+        dfs(1, 1, 1, 0);
+
+        bw.write(min + "\n");
+        bw.flush();
+    }
+
+    public static void dfs(int start, int now, int cnt, int cost) {
+
+        if (now == start && cost > 0) {
+            min = Math.min(min, cost);
+            return;
+        }
+
+        for (int n = 1; n <= N; n++) {
+            if (w[now][n] > 0) {
+                if (n == start && cnt == N) {
+                    cost += w[now][n];
+                    dfs(start, n, cnt + 1, cost);
+                } else if (!visited[n]) {
+                    visited[n] = true;
+                    cost += w[now][n];
+                    dfs(start, n, cnt + 1, cost);
+                    cost -= w[now][n];
+                    visited[n] = false;
+                }
+            }
+        }
+    }
+
 }
