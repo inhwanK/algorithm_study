dc6271af9dd83c6449b417f4ea92468cdafada7c
inhwanK <79566221+inhwanK@users.noreply.github.com>
2022-08-22 03:35:30 +0900

    Feat(Pro86491) : 풀이 중

---
+++ src/algorithm_study/programers/Pro86491.java
@@ -0,0 +1,13 @@
+package algorithm_study.programers;
+
+
+// 최소직사각형 - https://school.programmers.co.kr/learn/courses/30/lessons/86491
+public class Pro86491 {
+    public int solution(int[][] sizes) {
+        int answer = 0;
+
+        // 스위칭 하면서 가로 max, 세로 max
+        // 그 중 min, min;
+        return answer;
+    }
+}
