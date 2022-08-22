32e45c23efef21ed7d60565dec6773b4ba117b5d
inhwanK <79566221+inhwanK@users.noreply.github.com>
2022-08-23 00:35:50 +0900

    Feat(Pro86491) : 최소직사각형 풀이, 정답 추가

--- src/algorithm_study/programers/Pro86491.java
+++ src/algorithm_study/programers/Pro86491.java
@@ -6,8 +6,31 @@ public class Pro86491 {
     public int solution(int[][] sizes) {
         int answer = 0;
 
-        // 스위칭 하면서 가로 max, 세로 max
-        // 그 중 min, min;
+        int maxW = 0;
+        int maxH = 0;
+        for (int i = 0; i < sizes.length; i++) {
+            if (sizes[i][0] < sizes[i][1]) {
+                int temp = sizes[i][0];
+                sizes[i][0] = sizes[i][1];
+                sizes[i][1] = temp;
+            }
+            maxW = Math.max(maxW, sizes[i][0]);
+            maxH = Math.max(maxH, sizes[i][1]);
+        }
+
+        answer = maxW * maxH;
+        return answer;
+    }
+}
+
+class Pro86491Answer {
+    public int solution(int[][] sizes) {
+        int length = 0, height = 0;
+        for (int[] card : sizes) {
+            length = Math.max(length, Math.max(card[0], card[1]));
+            height = Math.max(height, Math.min(card[0], card[1]));
+        }
+        int answer = length * height;
         return answer;
     }
 }
