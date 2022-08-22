d6122dc42d3017fd199a3e47737dbb1d4f3dc300
inhwanK <79566221+inhwanK@users.noreply.github.com>
2022-08-23 02:38:09 +0900

    Feat(Leet21) : 풀이, 정답 추가

---
+++ src/algorithm_study/leetcode/Leet21.java
@@ -0,0 +1,42 @@
+package algorithm_study.leetcode;
+
+// 정답이긴해... 헷갈려서 문제...
+// Merge Two Sorted Lists - https://leetcode.com/problems/merge-two-sorted-lists/
+public class Leet21 {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        }
+
+        if (list2 == null) {
+            return list1;
+        }
+
+        if (list1.val < list2.val) {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists(list2.next, list1);
+        }
+        return list2;
+    }
+
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+}
