1c4e10c268d5c8ef27f821b5f37b845397b5fd62
inhwanK <79566221+inhwanK@users.noreply.github.com>
2022-10-06 22:31:12 +0900

    Feat(Pro67256): 키패드 누르기 풀이 추가

--- src/algorithm_study/programers/level1/Pro67256.java
+++ src/algorithm_study/programers/level1/Pro67256.java
@@ -2,4 +2,40 @@ package algorithm_study.programers.level1;
 
 // 키패드 누르기 - https://school.programmers.co.kr/learn/courses/30/lessons/67256
 public class Pro67256 {
+    public String solution(int[] numbers, String hand) {
+        String answer = "";
+        int left = 10;
+        int right = 12;
+
+        for (int tmp : numbers) {
+            if (tmp == 1 || tmp == 4 || tmp == 7) {
+                answer += "L";
+                left = tmp;
+            } else if (tmp == 3 || tmp == 6 || tmp == 9) {
+                answer += "R";
+                right = tmp;
+            } else {
+                if (tmp == 0) tmp = 11;
+                int leftdist = Math.abs(tmp - left) / 3 + Math.abs(tmp - left) % 3;
+                int rightdist = Math.abs(tmp - right) / 3 + Math.abs(tmp - right) % 3;
+
+                if (leftdist < rightdist) {
+                    answer += "L";
+                    left = tmp;
+                } else if (leftdist > rightdist) {
+                    answer += "R";
+                    right = tmp;
+                } else {
+                    if (hand.equals("left")) {
+                        answer += "L";
+                        left = tmp;
+                    } else {
+                        answer += "R";
+                        right = tmp;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
 }
