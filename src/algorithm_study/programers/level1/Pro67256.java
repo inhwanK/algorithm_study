8d9ffc2a3c49ff2c40fe1824a96d925776b87b5c
inhwanK <79566221+inhwanK@users.noreply.github.com>
2022-10-08 00:36:20 +0900

    Feat(Pro67256): 키패드 누르기 정답 추가

--- src/algorithm_study/programers/level1/Pro67256.java
+++ src/algorithm_study/programers/level1/Pro67256.java
@@ -106,3 +106,55 @@ class Pad {
         return Math.abs(this.x - pad.x) + Math.abs(this.y - pad.y);
     }
 }
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/67256/solution_groups?language=java
+class Pro67256Answer {
+    //        0부터 9까지 좌표 {y,x}
+    int[][] numpadPos = {
+            {3,1}, //0
+            {0,0}, //1
+            {0,1}, //2
+            {0,2}, //3
+            {1,0}, //4
+            {1,1}, //5
+            {1,2}, //6
+            {2,0}, //7
+            {2,1}, //8
+            {2,2}  //9
+    };
+    //초기 위치
+    int[] leftPos = {3,0};
+    int[] rightPos = {3,2};
+    String hand;
+    public String solution(int[] numbers, String hand) {
+        this.hand = (hand.equals("right")) ? "R" : "L";
+
+        String answer = "";
+        for (int num : numbers) {
+            String Umji = pushNumber(num);
+            answer += Umji;
+
+            if(Umji.equals("L")) {leftPos = numpadPos[num]; continue;}
+            if(Umji.equals("R")) {rightPos = numpadPos[num]; continue;}
+        }
+        return answer;
+    }
+
+    //num버튼을 누를 때 어디 손을 사용하는가
+    private String pushNumber(int num) {
+        if(num==1 || num==4 || num==7) return "L";
+        if(num==3 || num==6 || num==9) return "R";
+
+        // 2,5,8,0 일때 어디 손가락이 가까운가
+        if(getDist(leftPos, num) > getDist(rightPos, num)) return "R";
+        if(getDist(leftPos, num) < getDist(rightPos, num)) return "L";
+
+        //같으면 손잡이
+        return this.hand;
+    }
+
+    //해당 위치와 번호 위치의 거리
+    private int getDist(int[] pos, int num) {
+        return Math.abs(pos[0]-numpadPos[num][0]) + Math.abs(pos[1]-numpadPos[num][1]);
+    }
+}
