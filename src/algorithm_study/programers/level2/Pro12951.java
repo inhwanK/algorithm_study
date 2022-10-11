0d2000e6385621e40dfbb2922c97e82c3d824db6
inhwanK <79566221+inhwanK@users.noreply.github.com>
2022-10-12 00:13:38 +0900

    Feat(Pro12951): JadenCase 문자열 만들기 풀이 추가

--- src/algorithm_study/programers/level2/Pro12951.java
+++ src/algorithm_study/programers/level2/Pro12951.java
@@ -2,7 +2,34 @@ package algorithm_study.programers.level2;
 
 // JadenCase 문자열 만들기 - https://school.programmers.co.kr/learn/courses/30/lessons/12951
 public class Pro12951 {
-    public String solution(String s) {
+    public String solution_1(String s) {
+        String answer = "";
+
+        boolean flag = true;
+        String[] input = s.toLowerCase().split("");
+        for(int i = 0; i < input.length; i++) {
+            char[] arr = input[i].toCharArray();
+            if(flag) {
+                arr[0] = Character.toUpperCase(arr[0]);
+                flag = false;
+            }
+            if(arr[0] == ' ') {
+                flag = true;
+            }
+
+            String output = "";
+            for(int j = 0; j < arr.length; j++) {
+                output += arr[j];
+            }
+            input[i] = output;
+        }
+        for(int i = 0; i < input.length; i++) {
+            answer += input[i];
+        }
+        return answer;
+    }
+
+    public String solution_2(String s) {
         String answer = "";
 
         StringBuffer sb = new StringBuffer();
