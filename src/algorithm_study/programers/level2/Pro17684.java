f2eb8377a89633c4d796925ea2374c4f8a13819a
inhwanK <79566221+inhwanK@users.noreply.github.com>
2022-11-10 12:55:31 +0900

    Feat(Pro17684): [3차] 압축 정답 추가

--- src/algorithm_study/programers/level2/Pro17684.java
+++ src/algorithm_study/programers/level2/Pro17684.java
@@ -1,5 +1,34 @@
 package algorithm_study.programers.level2;
 
+import java.util.ArrayList;
+
 // [3차] 압축 - https://school.programmers.co.kr/learn/courses/30/lessons/17684
 public class Pro17684 {
+    public int[] solution(String msg) {
+        ArrayList<String> dic = new ArrayList<String>();
+        ArrayList<Integer> result = new ArrayList<Integer>();
+
+        for (int i = 0; i < 26; i++) {
+            dic.add(String.valueOf((char) ('A' + i)));
+        }
+
+        for (int i = 0; i < msg.length(); i++) {
+            for (int j = dic.size() - 1; j >= 0; j--) {
+                if (msg.substring(i).startsWith(dic.get(j))) {
+                    i += dic.get(j).length() - 1;
+                    result.add(j + 1);
+                    if (i + 1 < msg.length())
+                        dic.add(dic.get(j) + msg.charAt(i + 1));
+                    break;
+                }
+            }
+        }
+
+        int[] answer = new int[result.size()];
+
+        for (int i = 0; i < result.size(); i++)
+            answer[i] = result.get(i);
+
+        return answer;
+    }
 }
