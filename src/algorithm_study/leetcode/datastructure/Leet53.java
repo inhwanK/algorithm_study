7880648bdc6e0e286b49851a5268eaca62f71e80
inhwanK <inhan1009@naver.com>
2022-11-16 20:21:08 +0900

    Feat(Maximum Subarray): 풀이, 정답 추가

---
+++ src/algorithm_study/leetcode/datastructure/Leet53.java
@@ -0,0 +1,19 @@
+package algorithm_study.leetcode.datastructure;
+
+// Maximum Subarray - https://leetcode.com/problems/maximum-subarray/?envType=study-plan&id=data-structure-i
+public class Leet53 {
+    public int maxSubArray(int[] nums) {
+        // Kadane's algorithm (카데인의 알고리즘)
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            max = Math.max(max, sum);
+
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return max;
+    }
+}
