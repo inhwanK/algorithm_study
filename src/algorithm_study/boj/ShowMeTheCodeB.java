0e206d7f4cb6606a94bd67e8b880bbf074b8049a
inhwanK <79566221+inhwanK@users.noreply.github.com>
2023-01-06 20:19:19 +0900

    docs(ShowMeTheCodeB): 주석 제거

--- src/algorithm_study/boj/ShowMeTheCodeB.java
+++ src/algorithm_study/boj/ShowMeTheCodeB.java
@@ -6,7 +6,6 @@ import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 
-// 도넛 행성
 public class ShowMeTheCodeB {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
