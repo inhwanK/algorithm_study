9bf4159ff8ddb50b6972229b1407bbad3e76d80a
inhwanK <79566221+inhwanK@users.noreply.github.com>
2023-01-09 03:19:19 +0900

    feat(Pro64062): 징검다리 건너기 정답 추가

--- src/algorithm_study/programers/level3/Pro64062.java
+++ src/algorithm_study/programers/level3/Pro64062.java
@@ -39,4 +39,22 @@ public class Pro64062 {
 
         return min;
     }
+
+    // 좀 더 깔끔하게 푼 버전
+    public int solution_answer1(int[] stones, int k) {
+        int left = 1, right = 1 << 30, max = 0;
+        while(left <= right){
+            int mid = (left / 2) + (right / 2), cnt = 0;
+            for(int i : stones) {
+                cnt = (i - mid) < 0 ? cnt + 1 : 0;
+                if(cnt == k) break;
+            }
+            if(cnt < k) {
+                left = mid + 1;
+                max = Math.max(max, mid);
+            }
+            else right = mid - 1;
+        }
+        return max;
+    }
 }
