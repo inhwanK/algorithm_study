4538ce2b405bcfd6526a19b0fb7584d07d950720
inhwanK <inhan1009@naver.com>
2023-01-20 22:39:19 +0900

    feat(BOJ11399): ATM 정답 추가

--- src/algorithm_study/boj/greedy/BOJ11399.java
+++ src/algorithm_study/boj/greedy/BOJ11399.java
@@ -3,6 +3,8 @@ package algorithm_study.boj.greedy;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 import java.util.TreeMap;
 
 // 이거 TreeMap 아니면 안됨
@@ -31,11 +33,31 @@ public class BOJ11399 {
         int pre = 0;
         for (Integer key : map.keySet()) {
             for(int i = 0; i < map.get(key); i++) {
-
                 sum += key + pre;
                 pre += key;
             }
         }
         System.out.println(sum);
+    }
+
+    public static void answer1(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int[] p = new int[n];
+        for(int i = 0; i < n; ++i)
+            p[i] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(p); // 오름차순으로 정렬하는 경우가 걸리는 시간의 최솟값임
+
+        int sum = 0; // 각 사람이 돈을 인출하는데 필요한 시간
+        int time = 0; // 각 사람이 돈을 인출하는데 필요한 시간의 합
+        for(int i : p) {
+            sum = sum + i;
+            time += sum;
+        }
+
+        System.out.println(time);
     }
 }
