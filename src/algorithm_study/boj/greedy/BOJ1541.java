38df1953b93e1c5e9d5021273dd15dfd2f76155b
inhwanK <79566221+inhwanK@users.noreply.github.com>
2023-01-22 13:53:19 +0900

    feat(BOJ1541): 잃어버린 괄호 풀이, 정답 추가

--- src/algorithm_study/boj/greedy/BOJ1541.java
+++ src/algorithm_study/boj/greedy/BOJ1541.java
@@ -27,3 +27,29 @@ public class BOJ1541 {
         return sum;
     }
 }
+
+// 메인메소드에 저렇게할 수도 있었네..
+class BOJ1541Answer {
+    public static void main(String[] args) throws IOException {
+        BOJ1541Answer main = new BOJ1541Answer();
+        main.solve();
+    }
+    void solve() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int s = 0;
+        int r = 0;
+        boolean f = true;
+        for(int i = 0; i <= str.length(); i++) {
+            if(i == str.length() || str.charAt(i) == '+' || str.charAt(i) == '-') {
+                if(f)
+                    r += Integer.parseInt(str.substring(s, i));
+                else
+                    r -= Integer.parseInt(str.substring(s, i));
+                s = i+1;
+                if(i != str.length() && str.charAt(i) == '-') f= false;
+            }
+        }
+        System.out.println(r);
+    }
+}
