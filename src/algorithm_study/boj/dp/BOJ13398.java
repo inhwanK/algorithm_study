7263b036b2325928e1096907317c5d09e6a31873
inhwanK <79566221+inhwanK@users.noreply.github.com>
2023-01-23 04:46:19 +0900

    feat(BOJ13398): 연속합 2 정답 추가

--- src/algorithm_study/boj/dp/BOJ13398.java
+++ src/algorithm_study/boj/dp/BOJ13398.java
@@ -3,6 +3,7 @@ package algorithm_study.boj.dp;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 // 연속합 2 - https://www.acmicpc.net/problem/13398
 public class BOJ13398 {
@@ -35,5 +36,33 @@ public class BOJ13398 {
             max = Math.max(dpLeft[i - 1] + dpRight[i + 1], max);
         }
         System.out.println(max);
+    }
+
+    public static void answer1(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] num = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++)
+            num[i] = Integer.parseInt(st.nextToken());
+
+        int ans = num[0];
+        int[][] dp = new int[2][N];
+        dp[0][0] = num[0];
+        dp[1][0] = num[0];
+
+        for (int i = 1; i < N; i++) {
+            dp[0][i] = Math.max(dp[0][i - 1] + num[i], num[i]);
+            dp[1][i] = Math.max(dp[0][i - 1], dp[1][i - 1] + num[i]);
+
+            int temp = Math.max(dp[0][i], dp[1][i]);
+            if (temp > ans)
+                ans = temp;
+        }
+
+        System.out.print(ans);
+        br.close();
     }
 }
