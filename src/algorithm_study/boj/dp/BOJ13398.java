645b24f254ac17990a5a34280b3554f2a3be2ae2
inhwanK <79566221+inhwanK@users.noreply.github.com>
2023-01-23 04:45:19 +0900

    feat(BOJ13398): 연속합 2 풀이 추가

--- src/algorithm_study/boj/dp/BOJ13398.java
+++ src/algorithm_study/boj/dp/BOJ13398.java
@@ -11,22 +11,28 @@ public class BOJ13398 {
 
         int n = Integer.parseInt(br.readLine());
         int[] arr = new int[n];
+        int[] dpLeft = new int[n];
+        int[] dpRight = new int[n];
         String[] input = br.readLine().split(" ");
 
-        int result = 0;
         int max = 0;
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(input[i]);
         }
+        dpLeft[0] = max = arr[0];
+        dpRight[n - 1] = arr[n - 1];
 
-        for (int j = 0; j < n; j++) {
-            result += arr[j];
-            if (result <= 0) {
-                result = 0;
-                continue;
-            }
+        for (int i = 1; i < n; i++) {
+            dpLeft[i] = Math.max(dpLeft[i - 1] + arr[i], arr[i]);
+            max = Math.max(dpLeft[i], max);
+        }
 
-            max = Math.max(result, max);
+        for (int i = n - 2; i >= 0; i--) {
+            dpRight[i] = Math.max(dpRight[i + 1] + arr[i], arr[i]);
+        }
+
+        for (int i = 1; i < n - 1; i++) {
+            max = Math.max(dpLeft[i - 1] + dpRight[i + 1], max);
         }
         System.out.println(max);
     }
