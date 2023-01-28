62d540b968e0e86222434ad10cfe13d3832d8915
inhwanK <inhan1009@naver.com>
2023-01-28 17:21:19 +0900

    feat(BOJ11660): 구간 합 구하기 5 정답 추가

--- src/algorithm_study/boj/BOJ11660.java
+++ src/algorithm_study/boj/BOJ11660.java
@@ -1,6 +1,7 @@
 package algorithm_study.boj;
 
 import java.io.*;
+import java.util.StringTokenizer;
 
 // 구간 합 구하기 5 - https://www.acmicpc.net/problem/11660
 public class BOJ11660 {
@@ -35,5 +36,39 @@ public class BOJ11660 {
         }
         bw.flush();
         bw.close();
+    }
+}
+
+class BOJ11660Answer {
+    static int N, M;
+    static int[][] DP;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        DP = new int[N + 1][N + 1];
+        for(int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 1; j <= N; j++) {
+                int numbers = Integer.parseInt(st.nextToken());
+                DP[i][j] = DP[i-1][j] + DP[i][j-1] - DP[i-1][j-1] + numbers;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            sb.append(DP[x2][y2] - DP[x1 - 1][y2] - DP[x2][y1 - 1] + DP[x1 - 1][y1 - 1]).append("\n");
+        }
+
+        System.out.println(sb);
     }
 }
