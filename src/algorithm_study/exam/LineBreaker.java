c0cbcc8b8264ca447d6ff5d3a386e9b194d35005
inhwanK <79566221+inhwanK@users.noreply.github.com>
2023-02-05 00:54:25 +0900

    feat(LineBreaker): 자동 줄바꿈 프로그램 문제 추가

---
+++ src/algorithm_study/exam/LineBreaker.java
@@ -0,0 +1,14 @@
+package algorithm_study.exam;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 4. 자동 줄바꿈 프로그램
+public class LineBreaker {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = br.readLine();
+    }
+}
