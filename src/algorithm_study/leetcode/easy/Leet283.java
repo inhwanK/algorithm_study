28a76cce960c1c132da9e90f3d9d7c1fd0e5f8aa
inhwanK <inhan1009@naver.com>
2023-03-31 16:11:12 +0900

    feat(Leet283): More Zeroes 정답 추가

--- src/algorithm_study/leetcode/easy/Leet283.java
+++ src/algorithm_study/leetcode/easy/Leet283.java
@@ -30,4 +30,19 @@ public class Leet283 {
             }
         }
     }
+
+    // https://leetcode.com/problems/move-zeroes/solutions/172432/the-easiest-but-unusual-snowball-java-solution-beats-100-o-n-clear-explanation/
+    public void moveZeroesAnswer(int[] nums) {
+        int snowBallSize = 0;
+        for (int i=0;i<nums.length;i++){
+            if (nums[i]==0){
+                snowBallSize++;
+            }
+            else if (snowBallSize > 0) {
+                int t = nums[i];
+                nums[i]=0;
+                nums[i-snowBallSize]=t;
+            }
+        }
+    }
 }
