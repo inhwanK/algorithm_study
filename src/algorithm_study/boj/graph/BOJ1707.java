191a156ec8c68310f006fa2e554a6898db42d219
inhwanK <inhan1009@naver.com>
2023-05-26 17:51:39 +0900

    feat: 이분 그래프 풀이 추가

--- src/algorithm_study/boj/graph/BOJ1707.java
+++ src/algorithm_study/boj/graph/BOJ1707.java
@@ -8,37 +8,46 @@ import java.util.ArrayList;
 
 // 이분 그래프 - https://www.acmicpc.net/problem/1707
 public class BOJ1707 {
-
-    static int[][] board;
-    static int V;
-    static boolean flag;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int k = Integer.parseInt(br.readLine());
 
-        for(int i = 0; i < k; i++) {
-            String[] first = br.readLine().split(" ");
-            V = Integer.parseInt(first[0]);
-            int E = Integer.parseInt(first[1]);
+        while(k-- > 0) {
+            String[] testCase = br.readLine().split(" ");
+            int V = Integer.parseInt(testCase[0]);
+            int E = Integer.parseInt(testCase[1]);
 
-            board = new int[V + 1][V + 1];
+            ArrayList<Integer>[] arr = (ArrayList[]) new ArrayList[V + 1];
 
-            for(int j = 0; j < E; j++) {
+            for(int i = 1; i <= V; i++) {
+                arr[i] = new ArrayList();
+            }
+
+            for(int i = 0; i < E; i++) {
                 String[] line = br.readLine().split(" ");
                 int u = Integer.parseInt(line[0]);
                 int v = Integer.parseInt(line[1]);
 
-                board[u][v] = 1;
-                board[v][u] = 1;
+                arr[u].add(v);
+                arr[v].add(u);
             }
-            flag = false;
 
-            for(int ea = 1; ea <= V / 2 + 1; ea++) {
-                int[] left = new int[ea];
-//                int[] right = new int[V - ea];
-                boolean[] visited = new boolean[V];
-                dfs(0, ea, left, visited);
+            int[] color = new int[V + 1]; // 정점 방문 여부
+            boolean flag = true;
+
+            for(int i = 1; i <= V; i++) {
+                if(color[i] == 0) {
+                    dfs(arr, color, i, 1);
+                }
+            }
+
+            for(int i = 1; i <= V; i++) {
+                for(int j : arr[i]) {
+                    if(color[i] == color[j]) {
+                        flag = false;
+                    }
+                }
             }
 
             if(flag) {
@@ -47,57 +56,16 @@ public class BOJ1707 {
                 System.out.println("NO");
             }
         }
+
     }
 
-    private static void dfs(int depth, int size, int[] left, boolean[] visited) {
-        if(depth == size) {
-            if(validCheck(left, visited)) {
-                flag = true;
-            }
-            return;
-        }
-
-        for(int i = 1; i <= V; i++) {
-            if(!visited[i - 1]) {
-                left[depth] = i;
-                visited[i - 1] = true;
-                dfs(depth + 1, size, left, visited);
-                visited[i - 1] = false;
+    private static void dfs(ArrayList<Integer>[] arr, int[] color, int x, int c) {
+        color[x] = c;
+        for(int y : arr[x]) {
+            if(color[y] == 0) {
+                dfs(arr, color, y, 3 - c);
             }
         }
     }
 
-    private static boolean validCheck(int[] left, boolean[] visited) {
-        ArrayList<Integer> leftList = new ArrayList();
-        for(int i = 0; i < left.length; i++) {
-            leftList.add(left[i]);
-        }
-
-        ArrayList<Integer> rightList = new ArrayList();
-        for(int i = 0; i < visited.length; i++) {
-            if(!visited[i]) {
-                rightList.add(i + 1);
-            }
-        }
-
-        for(int i = 0; i < leftList.size(); i++) {
-            int target = leftList.get(i);
-            for(int j = 1; j <= V; j++) {
-                if(board[target][j] == 1 && leftList.contains(j)) {
-                    return false;
-                }
-            }
-        }
-
-        for(int i = 0; i < rightList.size(); i++) {
-            int target = rightList.get(i);
-            for(int j = 1; j <= V; j++) {
-                if(board[target][j] == 1 && rightList.contains(j)) {
-                    return false;
-                }
-            }
-        }
-
-        return true;
-    }
 }
