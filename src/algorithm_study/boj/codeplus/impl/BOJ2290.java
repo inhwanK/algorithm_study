ec61cf712551807759d1382e3b4af7fab1b683af
inhwanK <79566221+inhwanK@users.noreply.github.com>
2023-08-20 02:52:37 +0900

    feat(BOJ2290): LCT Test 풀이 중

---
+++ src/algorithm_study/boj/codeplus/impl/BOJ2290.java
@@ -0,0 +1,34 @@
+package algorithm_study.boj.codeplus.impl;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+// LCD Test - https://www.acmicpc.net/problem/2290
+public class BOJ2290 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+
+        int s = Integer.parseInt(input[0]);
+        char[] n = input[1].toCharArray();
+
+        char[][] result = new char[(s + 2) * n.length + n.length - 1][2 * s + 3];
+
+    }
+
+    private static void draw() {
+
+    }
+
+    private static void one(int col, int row, char[][] result) {
+
+    }
+    private static void two() {
+
+    }
+    private static void three() {
+
+    }
+}
