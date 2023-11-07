b7b082660bf40304774d9f8d9f43d97c78d17bc2
inhwanK <79566221+inhwanK@users.noreply.github.com>
2023-11-08 00:38:13 +0900

    update(Pro148653): solutionAnswer2 추가

--- src/algorithm_study/programers/level2/Pro148653.java
+++ src/algorithm_study/programers/level2/Pro148653.java
@@ -39,31 +39,90 @@ public class Pro148653 {
         }
         return result;
     }
-    
+
     public int solutionAnswer1(int storey) {
         String tmp = Integer.toString(storey);
         int[] arr = new int[tmp.length()];
-        for(int i=0; i<tmp.length(); i++) {
+        for (int i = 0; i < tmp.length(); i++) {
             arr[i] = tmp.charAt(i) - '0';
         }
 
         int answer = 0;
 
-        for(int i=arr.length-1; i>=0; i--) {
-            if(arr[i] > 5) {
-                answer += 10-arr[i];
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] > 5) {
+                answer += 10 - arr[i];
 
-                if(i==0) answer++;
-                else arr[i-1]++;
-            }
-            else if(arr[i]==5 && i>0 && arr[i-1]>=5) {
-                arr[i-1]++;
+                if (i == 0) answer++;
+                else arr[i - 1]++;
+            } else if (arr[i] == 5 && i > 0 && arr[i - 1] >= 5) {
+                arr[i - 1]++;
                 answer += 5;
-            }
-            else {
+            } else {
                 answer += arr[i];
             }
 
+        }
+
+        return answer;
+    }
+
+    public int solutionAnswer2(int storey) {
+
+        //마법의 엘리베이터
+
+        //절댓값이 10 ^ c ( c >= 0 인 정수)
+        //더한 값으로 이동
+        //0보다 작으면 엘리베이터는 움직이지 않습니다.
+        //0층이 가장 아래층 -> 엘리베이터 와 민수 모두 0층
+
+        //절반보다 크면 더해서 큰 값을 만드는게 이득
+        //절반보다 작으면 빼서 작은 값을 만드는게 이득
+
+        int answer = 0;
+
+        char[] chars = String.valueOf(storey).toCharArray();
+        int[] ints = new int[chars.length];
+
+        // 숫자를 문자열로 바꿨다가 다시 int 배열로 변경
+        for (int i = 0; i < chars.length; i++) {
+            ints[i] = chars[i] - '0';
+        }
+
+        for (int i = ints.length - 1; i >= 0; i--) {
+            int anInt = ints[i];
+
+            // 6 이상이므로 앞자리수 증가.
+            if (anInt >= 6) {
+
+                answer += (10 - anInt);
+                if (i == 0) {
+                    answer += 1;
+                    continue;
+                }
+
+                ints[i - 1]++;
+
+                // 6 이하일 경우 앞자리수가 그대로.
+            } else if (anInt <= 4) {
+                answer += anInt;
+            } else { // 5인 경우
+
+                // 마지막 자리수인 경우 그냥 answer 증가시키고 끝
+                if (i == 0) {
+                    answer += 5;
+                    continue;
+                }
+
+                // 마지막 자릿 수 아닌 경우 5보다 작으면 현재 자릿수만큼만 계산
+                if (ints[i - 1] < 5) {
+                    answer += anInt;
+                } else {
+                    // 5보다 크면 앞자리수 증가
+                    answer += 5;
+                    ints[i - 1]++;
+                }
+            }
         }
 
         return answer;
