c590f4dfdcbd686836404749b70e4a5a97faedca
inhwanK <79566221+inhwanK@users.noreply.github.com>
2024-03-22 00:58:30 +0900

    update(Pro49191): 순위 다른 풀이 추가

--- src/algorithm_study/programers/level3/Pro49191.java
+++ src/algorithm_study/programers/level3/Pro49191.java
@@ -45,4 +45,40 @@ public class Pro49191 {
 
         return answer;
     }
+
+    // true, false 체크로 풀기
+    public int solution2(int n, int[][] results) {
+        int answer = 0;
+
+        boolean[][] chk = new boolean[n + 1][n + 1];
+
+        for(int i = 0; i < results.length; i++) {
+            chk[results[i][0]][results[i][1]] = true;
+        }
+
+        for(int k = 1; k < n + 1; k++) {
+            for(int i = 1; i < n + 1; i++) {
+                for(int j = 1; j < n + 1; j++) {
+                    if(i != j && chk[i][k] && chk[k][j]) {
+                        chk[i][j] = true;
+                    }
+                }
+            }
+        }
+
+        for(int i = 1; i < n + 1; i++) {
+            boolean pass = true;
+            for(int j = 1; j < n + 1; j++) {
+                if(i != j && !(chk[i][j] || chk[j][i])) {
+                    pass = false;
+                    break;
+                }
+            }
+            if(pass) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
 }
