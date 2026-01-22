f359f14dc239f53df6c69d0adc5f4ed94d315650
inhwanK <inhan1009@naver.com>
2026-01-22 23:54:45 +0900

    feat(Leet2817): Minimum Absolute Difference Between Elements With Constraint 풀이 추가

---
+++ src/algorithm_study/leetcode/medium/Leet2817.java
@@ -0,0 +1,5 @@
+package algorithm_study.leetcode.medium;
+
+// 2817. Minimum Absolute Difference Between Elements With Constraint - https://leetcode.com/problems/minimum-absolute-difference-between-elements-with-constraint/description/?envType=problem-list-v2&envId=vol6mrd1
+public class Leet2817 {
+}
