3fc60b12a5b149c0a5f39c1809cf726e33bc56ab
inhwanK <inhan1009@naver.com>
2026-02-03 23:53:30 +0900

    feat(Leet1027): Longest Arithmetic Subsequence 풀이 추가

---
+++ src/algorithm_study/leetcode/medium/Leet1027.java
@@ -0,0 +1,5 @@
+package algorithm_study.leetcode.medium;
+
+// 1027. Longest Arithmetic Subsequence - https://leetcode.com/problems/longest-arithmetic-subsequence/description/?envType=problem-list-v2&envId=vol6mrd1
+public class Leet1027 {
+}
