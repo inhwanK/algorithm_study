d3429ae1f607f78024abdc453f20e4d7b0871763
inhwanK <inhan1009@naver.com>
2026-02-24 19:56:05 +0900

    feat(Leet877): Stone Game 풀이 추가

---
+++ src/algorithm_study/leetcode/medium/Leet877.java
@@ -0,0 +1,8 @@
+package algorithm_study.leetcode.medium;
+
+// 877. Stone Game - https://leetcode.com/problems/stone-game/description/
+public class Leet877 {
+    public boolean stoneGame(int[] piles) {
+        return true;
+    }
+}
