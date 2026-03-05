cd825769d857d06b99959c6fbbef346e0862ff6a
inhwanK <inhan1009@naver.com>
2026-03-05 23:53:43 +0900

    feat(Leet1049): Last Stone Weight II 풀이 추가

--- src/algorithm_study/leetcode/medium/Leet1049.java
+++ src/algorithm_study/leetcode/medium/Leet1049.java
@@ -2,4 +2,8 @@ package algorithm_study.leetcode.medium;
 
 // 1049. Last Stone Weight II - https://leetcode.com/problems/last-stone-weight-ii/description/?envType=problem-list-v2&envId=vol6mrd1
 public class Leet1049 {
+    public int lastStoneWeightII(int[] stones) {
+        int ans = 0;
+        return ans;
+    }
 }
